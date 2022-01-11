fcdc2f3a1245c351251ef60a9f21635bb1a19c1c
IgetyourgoatC110181113 <93572309+IgetyourgoatC110181113@users.noreply.github.com>
2022-01-11 14:28:02 +0800

    Add files via upload

--- untitled123/src/main/java/ouo.java
+++ untitled123/src/main/java/ouo.java
@@ -4,8 +4,6 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class ouo {
     public static void main(String[] args) throws IOException {
@@ -21,7 +19,6 @@ public class ouo {
             System.out.println("======="+head.text()+"======="); //channel_menu sticky_position_with_15
 
             Elements boards = index_1.select("div.indexWp_page__left a");
-            //System.out.println(boards.text());
             for(Element w : boards){
                 String sum_boards_text = w.text();
                 URL_board = URL_1 + w.attr("href");
@@ -33,11 +30,7 @@ public class ouo {
             System.out.println();
             System.out.println("============<最新看板>============");
             System.out.println();
-            int num = 0;
-
-            if(num == 0) {
-                for (Element x : tbody) {
-                    Elements tr = tbody.select("tr");
+             Elements tr = tbody.select("tr");
                     for (Element y : tr) {
                         Elements td = y.select("td a");
                         URL_2 = URL_1 + td.attr("href");
@@ -48,29 +41,7 @@ public class ouo {
                         Element views = index_2.select("div.views").first();
                         Element replys = index_2.select("div.replys").first();
                         System.out.println("===>" + views.text() + "  " + replys.text());
-
-                        Elements USER = index_2.select("div[class~=^Fxg]");
-                        }
-                    }
                 }
-            //URL_2 = URL_1 + td.attr("href");
-            //index_2 = Jsoup.connect(URL_2).get();
-            //System.out.printf("%s",index_2.title()+" ");
-            //System.out.println(URL_2);
-            //for (Element title : titles) {
-            //String link = title.attr("href");
-            //System.out.println("Link: " + link);
-            //Document innerDoc = Jsoup.connect("https://ck101.com/" + link).get();
-            //String titleText = innerDoc.select("h1").text();
-            //String viewNumber = innerDoc.select("div.views").text();
-            //String commentNumber = innerDoc.select("div.replys").text();
-            //System.out.println("titleText: " + titleText);
-            //System.out.println("viewNumber: " + viewNumber);
-            //System.out.println("commentNumber: " + commentNumber);
-            //Elements tags = innerDoc.select("div.tagBox a");
-            //System.out.print("tag: ");
-            //for (Element tag : tags) {
-            //System.out.print(tag.text() + " ,");
         } catch (Exception e) {
             e.printStackTrace();
         }
